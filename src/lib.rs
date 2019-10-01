0aa180f555f46fe77693564e8bfc1fd620887646
dashaw92 <dashaw92@gmail.com>
2019-10-01 15:23:22 -0400

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::collections::hash_map::{HashMap, Entry};
+use std::collections::hash_map::{Entry, HashMap};
 
-#[derive(Clone,Debug)]
+#[derive(Clone, Debug)]
 pub enum Command {
     MovePointerRight,
     MovePointerLeft,
@@ -23,7 +23,7 @@ impl Command {
             ',' => Some(Self::Input),
             '[' => Some(Self::JumpStart),
             ']' => Some(Self::JumpEnd),
-            _ => None
+            _ => None,
         }
     }
 }
@@ -38,9 +38,7 @@ struct Program {
 }
 
 fn get_commands(s: &str) -> Vec<Command> {
-    s.chars()
-        .filter_map(Command::from_char)
-        .collect()
+    s.chars().filter_map(Command::from_char).collect()
 }
 
 pub fn run(s: &str) -> String {
@@ -49,7 +47,7 @@ pub fn run(s: &str) -> String {
         pointer: 0,
         jumps: Vec::new(),
         output: Vec::new(),
-        commands: get_commands(s)
+        commands: get_commands(s),
     };
 
     let mut i = 0;
@@ -57,56 +55,48 @@ pub fn run(s: &str) -> String {
         match prgm.commands[i] {
             Command::MovePointerRight => {
                 prgm.pointer += 1;
-            },
+            }
 
             Command::MovePointerLeft => {
                 prgm.pointer -= 1;
-            },
+            }
 
-            Command::Increment => {
-                *prgm.tape.entry(prgm.pointer).or_insert(0) += 1
-            },
+            Command::Increment => *prgm.tape.entry(prgm.pointer).or_insert(0) += 1,
 
-            Command::Decrement => {
-                match prgm.tape.entry(prgm.pointer) {
-                    Entry::Occupied(mut entry) => {
-                        if *entry.get() != 0 {
-                            *entry.get_mut() -= 1;
-                        }
-                    },
+            Command::Decrement => match prgm.tape.entry(prgm.pointer) {
+                Entry::Occupied(mut entry) => {
+                    if *entry.get() != 0 {
+                        *entry.get_mut() -= 1;
+                    }
+                }
 
-                    Entry::Vacant(entry) => {
-                        entry.insert(0);
-                    },
+                Entry::Vacant(entry) => {
+                    entry.insert(0);
                 }
             },
 
-            Command::Output => {
-                prgm.output.push(*prgm.tape.get(&prgm.pointer).unwrap_or(&0) as char)
-            },
+            Command::Output => prgm
+                .output
+                .push(*prgm.tape.get(&prgm.pointer).unwrap_or(&0) as char),
 
-            Command::Input => {
-
-            },
+            Command::Input => {}
 
             Command::JumpStart => {
                 prgm.jumps.push(i);
-            },
+            }
 
-            Command::JumpEnd => {
-                match prgm.tape.entry(prgm.pointer) {
-                    Entry::Occupied(entry) => {
-                        if *entry.get() != 0 {
-                            if !prgm.jumps.is_empty() {
-                                i = *prgm.jumps.last().unwrap();
-                            }
-                        } else {
-                            prgm.jumps.pop();
+            Command::JumpEnd => match prgm.tape.entry(prgm.pointer) {
+                Entry::Occupied(entry) => {
+                    if *entry.get() != 0 {
+                        if !prgm.jumps.is_empty() {
+                            i = *prgm.jumps.last().unwrap();
                         }
-                    },
-
-                    _ => {},
+                    } else {
+                        prgm.jumps.pop();
+                    }
                 }
+
+                _ => {}
             },
         }
 
